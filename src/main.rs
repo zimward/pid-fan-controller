6dca730126fcd4252595abe63e3fef8cef1a483c
zimward <96021122+zimward@users.noreply.github.com>
2022-07-12 17:31:23 +0200

    Added fn to only read x bytes to prevent large buffered read from read_strings + removed panic! to signal errors to the user

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use pid::PID;
 use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::fmt::Debug;
-use std::fs::{read_to_string, write};
-use std::panic;
+use std::fs::{read_to_string, write, File};
+use std::io::Read;
 use std::path::PathBuf;
 use std::string::String;
 use std::thread;
@@ -14,11 +14,43 @@ use std::time::Duration;
 
 const CONFIG_FILE: &'static str = "/etc/pid-fan-settings.json";
 
+trait InformedCrash<T> {
+    fn unexpect(self, message: &str) -> T;
+}
+
+impl<T, E> InformedCrash<T> for Result<T, E> {
+    fn unexpect(self, message: &str) -> T {
+        if let Ok(val) = self {
+            return val;
+        } else {
+            eprintln!("{}", message);
+            std::process::exit(0xFF);
+        }
+    }
+}
+impl<T> InformedCrash<T> for Option<T> {
+    fn unexpect(self, message: &str) -> T {
+        if let Some(val) = self {
+            return val;
+        } else {
+            eprintln!("{}", message);
+            std::process::exit(0xFF);
+        }
+    }
+}
+
 #[derive(Debug)]
 struct HeatSrc {
     temp_input: PathBuf,
     pub last_pid: f32,
     pid: PID,
+}
+
+fn read_c(path: &PathBuf, count: usize) -> Result<String, std::string::FromUtf8Error> {
+    let mut file = File::open(path).unwrap();
+    let mut buf = vec![0u8; count];
+    let bytes_read = file.read(&mut buf).unwrap();
+    String::from_utf8(buf[..bytes_read].to_vec())
 }
 
 impl HeatSrc {
@@ -30,8 +62,8 @@ impl HeatSrc {
         }
     }
     pub fn run_pwm(&mut self, interval: f32) {
-        let mut tmp = read_to_string(&self.temp_input).expect("Failed to read temperature");
-        tmp.remove(tmp.len() - 1);
+        let mut tmp = read_c(&self.temp_input, 7).unexpect("Failed to read temperature");
+        tmp.pop();
         let temp: f32 = tmp.parse().unwrap();
         self.last_pid = self.pid.run(temp, interval);
     }
@@ -118,12 +150,14 @@ fn get_object(value: JsonValue) -> Option<JsonObject> {
 }
 
 fn resolve_file_path(path: String) -> PathBuf {
-    let iter = glob(path.as_str()).expect("Failed to process glob");
+    let iter = glob(path.as_str()).unexpect("Failed to process glob");
     let paths: Vec<Result<PathBuf, GlobError>> = iter.collect();
     if paths.len() > 1 {
-        panic!("Path {} returns more than one result.", path.as_str());
+        eprintln!("Path {} returns more than one result.", path.as_str());
+        std::process::exit(0xFF);
     } else if paths.len() == 0 {
-        panic!("Path {} returns no vaild result.", path.as_str());
+        eprintln!("Path {} returns no vaild result.", path.as_str());
+        std::process::exit(0xFF);
     }
     paths[0].as_ref().unwrap().to_path_buf()
 }
@@ -138,35 +172,36 @@ fn handle_srcs(srcs: Vec<JsonValue>) -> Vec<(String, HeatSrc)> {
         let mut i: f32 = 0.0;
         let mut d: f32 = 0.0;
 
-        let src = get_object(src).expect("heat sources have to be defined as objects");
+        let src = get_object(src).unexpect("heat sources have to be defined as objects");
         for e in src {
             let k: String = e.0.into_iter().collect();
             match k.as_str() {
                 "name" => {
-                    name = get_string(e.1).expect("'name' of heat source must be a string");
+                    name = get_string(e.1).unexpect("'name' of heat source must be a string");
                 }
                 "wildcard_path" => {
                     temp_input =
-                        get_string(e.1).expect("'wildcard_path' of heat source must be a string");
+                        get_string(e.1).unexpect("'wildcard_path' of heat source must be a string");
                 }
                 "PID_params" => {
-                    let params = get_object(e.1).expect("'PID_params' has to be an Object");
+                    let params = get_object(e.1).unexpect("'PID_params' has to be an Object");
                     for e in params {
                         let k: String = e.0.into_iter().collect();
                         match k.as_str() {
                             "set_point" => {
                                 //the sysfs reading is in m°C
-                                set_point =
-                                    get_number(e.1).expect("'set_point' must be a number") * 1000.0;
+                                set_point = get_number(e.1)
+                                    .unexpect("'set_point' must be a number")
+                                    * 1000.0;
                             }
                             "P" => {
-                                p = get_number(e.1).expect("'P' must be a number");
+                                p = get_number(e.1).unexpect("'P' must be a number");
                             }
                             "I" => {
-                                i = get_number(e.1).expect("'I' must be a number");
+                                i = get_number(e.1).unexpect("'I' must be a number");
                             }
                             "D" => {
-                                d = get_number(e.1).expect("'D' must be a number");
+                                d = get_number(e.1).unexpect("'D' must be a number");
                             }
                             &_ => {}
                         }
@@ -192,37 +227,39 @@ fn handle_fans(fans: Vec<JsonValue>) -> Vec<(String, u32, u32, bool, Vec<String>
         let mut max_pwm: u32 = 255;
         let mut cutoff: bool = false;
         let mut heat_srcs: Option<Vec<String>> = None;
-        let fan = get_object(e).expect("fan entries have to be objects");
+        let fan = get_object(e).unexpect("fan entries have to be objects");
         for e in fan {
             let k: String = e.0.into_iter().collect();
             match k.as_str() {
                 "wildcard_path" => {
                     wildcard_path =
-                        get_string(e.1).expect("'wildcard_path' of fan has to be a string");
+                        get_string(e.1).unexpect("'wildcard_path' of fan has to be a string");
                 }
                 "min_pwm" => {
-                    min_pwm = get_integer(e.1).expect("'min_pwm' has to be a integer") as u32;
+                    min_pwm = get_integer(e.1).unexpect("'min_pwm' has to be a integer") as u32;
                 }
                 "max_pwm" => {
-                    max_pwm = get_integer(e.1).expect("'max_pwm' has to be a integer") as u32;
+                    max_pwm = get_integer(e.1).unexpect("'max_pwm' has to be a integer") as u32;
                 }
                 "cutoff" => {
                     if let JsonValue::Boolean(val) = e.1 {
                         cutoff = val;
                     } else {
-                        panic!("'cutoff' has to be a boolean");
+                        eprintln!("'cutoff' has to be a boolean");
+                        std::process::exit(0xFF);
                     }
                 }
                 "heat_pressure_srcs" => {
-                    let srcs = get_array(e.1).expect("'heat_pressure_srcs' has to be a array");
+                    let srcs = get_array(e.1).unexpect("'heat_pressure_srcs' has to be a array");
                     if heat_srcs == None {
                         heat_srcs = Some(Vec::with_capacity(srcs.len()));
                     }
                     for src in srcs {
                         if let Some(heat_srcs) = heat_srcs.borrow_mut() {
                             heat_srcs.push(
-                                get_string(src)
-                                    .expect("'heat_pressure_srcs' array may only contain strings"),
+                                get_string(src).unexpect(
+                                    "'heat_pressure_srcs' array may only contain strings",
+                                ),
                             );
                         }
                     }
@@ -231,23 +268,25 @@ fn handle_fans(fans: Vec<JsonValue>) -> Vec<(String, u32, u32, bool, Vec<String>
             }
         }
         if wildcard_path.len() == 0 {
-            panic!("'wildcard_path' is a mandatory fan parameter");
+            eprintln!("'wildcard_path' is a mandatory fan parameter");
+            std::process::exit(0xFF);
         }
-        let srcs = heat_srcs.expect("fan must have 'heat_pressure_srcs'");
+        let srcs = heat_srcs.unexpect("fan must have 'heat_pressure_srcs'");
         configured_fans.push((wildcard_path, min_pwm, max_pwm, cutoff, srcs));
     }
     configured_fans
 }
 
 fn parse_config() -> (Vec<HeatSrc>, Vec<Fan>, u32) {
-    let config = match parse_json(&read_to_string(CONFIG_FILE).expect("Error reading config file"))
-    {
-        Ok(cfg) => cfg,
-        Err(_err) => {
-            panic!("Error parsing config file.");
-        }
-    };
-    let cfg = get_object(config).expect("config must be wrap in an object");
+    let config =
+        match parse_json(&read_to_string(CONFIG_FILE).unexpect("Error reading config file")) {
+            Ok(cfg) => cfg,
+            Err(_err) => {
+                eprintln!("Error parsing config file.");
+                std::process::exit(0xFF);
+            }
+        };
+    let cfg = get_object(config).unexpect("config must be wrap in an object");
     let mut heat_srcs: Vec<(String, HeatSrc)> = Vec::new();
     let mut fans: Vec<(String, u32, u32, bool, Vec<String>)> = Vec::new();
     let mut interval: u32 = 500;
@@ -255,16 +294,16 @@ fn parse_config() -> (Vec<HeatSrc>, Vec<Fan>, u32) {
         let typ: String = e.0.into_iter().collect();
         match typ.as_str() {
             "heat_srcs" => {
-                heat_srcs = handle_srcs(get_array(e.1).expect("'heat_srcs' must be a array"));
+                heat_srcs = handle_srcs(get_array(e.1).unexpect("'heat_srcs' must be a array"));
             }
             "fans" => {
-                fans = handle_fans(get_array(e.1).expect("'fans' mut be a array"));
+                fans = handle_fans(get_array(e.1).unexpect("'fans' mut be a array"));
             }
             "interval" => {
                 interval = get_integer(e.1)
-                    .expect("'interval' must be a number")
+                    .unexpect("'interval' must be a number")
                     .try_into()
-                    .expect("interval must be positive.");
+                    .unexpect("interval must be positive.");
             }
             &_ => {}
         }
@@ -281,7 +320,7 @@ fn parse_config() -> (Vec<HeatSrc>, Vec<Fan>, u32) {
         for src in heat_pressure_srcs {
             let k = name_lookup
                 .get(&src)
-                .expect("heat_pressure_src entry is wrong");
+                .unexpect("heat_pressure_src entry is wrong");
             heat_prs_srcs.push(*k);
         }
         fin_fans.push(Fan::new(
@@ -296,12 +335,6 @@ fn parse_config() -> (Vec<HeatSrc>, Vec<Fan>, u32) {
 }
 
 fn main() {
-    panic::set_hook(Box::new(|info| {
-        let s = info.to_string();
-        let (_, s) = s.split_at(s.find("'").unwrap() + 1);
-        let (s, _) = s.split_at(s.rfind("'").unwrap());
-        println!("{}", s);
-    }));
     let (mut heat_srcs, fans, interval) = parse_config();
     let interval_seconds: f32 = (interval as f32) / 1000.0;
     let mut enable = true;
