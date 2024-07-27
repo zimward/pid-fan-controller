499cae761a3405052cafc7b2c60ffe4b1c36972c
zimward <96021122+zimward@users.noreply.github.com>
2024-07-27 13:41:57 +0200

    applied nixfmt rfc style

--- flake.nix
+++ flake.nix
@@ -5,19 +5,22 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    systems = ["x86_64-linux" "aarch64-linux"];
-    forAllSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
-  in {
-    packages = forAllSystem (system: {
-      default = nixpkgs.legacyPackages.${system}.callPackage ./package.nix {};
-    });
-    nixosModules = {
-      pid-fan-controller = import ./settings.nix self;
-      default = self.nixosModules.pid-fan-controller;
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystem (system: {
+        default = nixpkgs.legacyPackages.${system}.callPackage ./package.nix { };
+      });
+      nixosModules = {
+        pid-fan-controller = import ./settings.nix self;
+        default = self.nixosModules.pid-fan-controller;
+      };
     };
-  };
 }
